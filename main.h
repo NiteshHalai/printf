bdd9002a500f842b207eb60184fe3e6d6487be14
sameers91 <sammoosa.sm@gmail.com>
2022-10-18 21:11:23 +0300

    updated main.h (putchar and printf)
    to include printf and putchar functions

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*list functions here*/
+int _printf(const char *format, ...);
+int new_putchar(char);
+
+#endif
