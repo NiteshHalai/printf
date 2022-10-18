8c88aa9abc8025228c37a0519c2217560fe87f6e
NiteshHalai <nitesh.halai@gmail.com>
2022-10-18 20:16:53 +0300

    Update printf.c

---
+++ printf.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+
+{
+	va_list	ap;
+	int		i;
+	int		ct;
+
+	i = 0;
+	ct = 0;
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] != '%')
+			putchar(format[i]);
+    
+		i++;
+	}
+  
+	va_end(ap);
+	return (0);
+}
