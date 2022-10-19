a72b9b93eabd5c7483b0c1060b65d04ec9f03846
NiteshHalai <nitesh.halai@gmail.com>
2022-10-19 23:18:40 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,14 +6,17 @@ int _printf(const char *format, ...)
 	va_list	ap;
 	int	i;
 	int j;
+	int count;
 
 	i = 0;
+	count = 0;
 
 	va_start(ap, format);
 	while (format[i])
 	{
-	 if (format[i] != '%'){
-	new_putchar(format[i]);
+	    if (format[i] != '%'){
+			  new_putchar(format[i]);
+			  count++;
 	      
 	    }
 			
@@ -22,13 +25,14 @@ int _printf(const char *format, ...)
         if (format[i] == 'c'){
         j = va_arg (ap, int);  
         new_putchar(j);
+        count++;
         }
           
       }
       i++;
 	}
-  
+
 	va_end(ap);
-	return (0);
-	
+
+	return (count);
 }
