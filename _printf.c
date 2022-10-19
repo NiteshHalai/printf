15e7db9447460be21e0d33a77c8cdec09fb2fd27
NiteshHalai <nitesh.halai@gmail.com>
2022-10-19 23:04:56 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,24 +4,28 @@ int _printf(const char *format, ...)
 
 {
 	va_list	ap;
-	int i;
-	
+	int	i;
+	int j;
+
 	i = 0;
 
 	va_start(ap, format);
 	while (format[i])
 	{
-		if (format[i] != '%'){
-		new_putchar(format[i]);
-	      }
+	 if (format[i] != '%'){
+	new_putchar(format[i]);
+	      
+	    }
 			
-		else if (format[i] == '%'){ 
-			i++;
-			if (format[i] == 'c'){
-				new_putchar('H');
-			}
-		}
-      		i++;
+      else if (format[i] == '%'){
+        i++;
+        if (format[i] == 'c'){
+        j = va_arg (ap, int);  
+        new_putchar(j);
+        }
+          
+      }
+      i++;
 	}
   
 	va_end(ap);
