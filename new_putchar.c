093df3d9466c930dcf596adbb3bc5812017f38c8
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 17:34:20 +0300

    Update new_putchar.c

--- new_putchar.c
+++ new_putchar.c
@@ -1,26 +1,12 @@
-#include "main.h"
 
 /**
  * new_putchar- writes the character c to stdout.
  * @c: The character to print
  *
  * Return: On success 1.
- * got from stackoverflow.. and some random dude's code
+ * got from stackoverflow..
  */
 int new_putchar(char c)
 {
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
