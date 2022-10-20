bfae3491c7542a6e684ab6cfac7ce9cb893392cb
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 17:38:27 +0300

    Update new_putchar.c

--- new_putchar.c
+++ new_putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * new_putchar- writes the character c to stdout.
@@ -7,7 +8,20 @@
  * Return: On success 1.
  * got from stackoverflow..
  */
-int new_putchar(char c)
+int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
