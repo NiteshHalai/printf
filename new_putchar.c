374600aea280ba0b7e3059c377466a843c1a3091
NiteshHalai <nitesh.halai@gmail.com>
2022-10-20 17:33:13 +0300

    Update new_putchar.c

--- new_putchar.c
+++ new_putchar.c
@@ -5,9 +5,22 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * got from stackoverflow..
+ * got from stackoverflow.. and some random dude's code
  */
 int new_putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
